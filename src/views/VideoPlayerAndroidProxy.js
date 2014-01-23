5c2b01d251e65ced3673070b7320d4374f94574e
ccpony86 <19461718@qq.com>
2014-01-23 10:38:25 +0800

    update

---
+++ src/views/VideoPlayerAndroidProxy.js
@@ -0,0 +1,32 @@
+/*global window, console, clearInterval, setInterval, document, unescape, L, webkitURL*/
+/*
+ * VideoPlayer represents a Licode video component that shows either a local or a remote video.
+ * Ex.: var player = VideoPlayer({id: id, stream: stream, elementID: elementID});
+ * A VideoPlayer is also a View component.
+ */
+var Erizo = Erizo || {};
+Erizo.VideoPlayer = function (spec) {
+    "use strict";
+
+    var that = {};
+
+    // Variables
+
+    // VideoPlayer ID
+    that.id = spec.id;
+
+    // Stream that the VideoPlayer will play
+    that.stream = spec.stream.stream;
+
+    // DOM element in which the VideoPlayer will be appended
+    that.elementID = spec.elementID;
+
+    // Public functions
+
+    // It will stop the VideoPlayer and remove it from the HTML
+    that.destroy = function () {        
+    	return pcManagerJS.call_method(PCManagerJS.method_map['delete_pc'], that.stream.pc_id, {});
+    };
+
+    return that;
+};
--- src/webkitRTCPeerConnection/webkitRTCPeerConnection.js
+++ src/webkitRTCPeerConnection/webkitRTCPeerConnection.js
@@ -6,54 +6,54 @@ webkitRTCPeerConnection = function() {
 	this.pc_id = pcManagerJS.new_pc(this);
 };
 
-webkitRTCPeerConnection.prototype.addStream = function(stream) {
+webkitRTCPeerConnection.addStream = function(stream) {
 	stream.pc_id = this.pc_id;
 	
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['addStream'], this.pc_id, stream);
+	return pcManagerJS.call_method(PCManagerJS.method_map['addStream'], this.pc_id, stream);
 };
 
-webkitRTCPeerConnection.prototype.removeStream = function(stream) {	
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['removeStream'], this.pc_id, stream);
+webkitRTCPeerConnection.removeStream = function(stream) {	
+	return pcManagerJS.call_method(PCManagerJS.method_map['removeStream'], this.pc_id, stream);
 };
 
-webkitRTCPeerConnection.prototype.close = function() {
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['close'], this.pc_id, {});
+webkitRTCPeerConnection.close = function() {
+	return pcManagerJS.call_method(PCManagerJS.method_map['close'], this.pc_id, {});
 };
 
-webkitRTCPeerConnection.prototype.createAnswer = function(callback, obj, constraints) {
+webkitRTCPeerConnection.createAnswer = function(callback, obj, constraints) {
 	this.createAnswer_callback = callback;
 	
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['createAnswer'], this.pc_id, constraints);
+	return pcManagerJS.call_method(PCManagerJS.method_map['createAnswer'], this.pc_id, constraints);
 };
 
-webkitRTCPeerConnection.prototype.createOffer = function(callback, obj, constraints) {
+webkitRTCPeerConnection.createOffer = function(callback, obj, constraints) {
 	this.createOffer_callback = callback;
 	
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['createOffer'], this.pc_id, constraints);
+	return pcManagerJS.call_method(PCManagerJS.method_map['createOffer'], this.pc_id, constraints);
 };
 
-webkitRTCPeerConnection.prototype.createDataChannel = function(param) {
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['createDataChannel'], this.pc_id, param);
+webkitRTCPeerConnection.createDataChannel = function(param) {
+	return pcManagerJS.call_method(PCManagerJS.method_map['createDataChannel'], this.pc_id, param);
 };
 
-webkitRTCPeerConnection.prototype.setLocalDescription = function(sd) {
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['setLocalDescription'], this.pc_id, sd);
+webkitRTCPeerConnection.setLocalDescription = function(sd) {
+	return pcManagerJS.call_method(PCManagerJS.method_map['setLocalDescription'], this.pc_id, sd);
 };
 
-webkitRTCPeerConnection.prototype.setRemoteDescription = function(sd) {
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['setRemoteDescription'], this.pc_id, sd);
+webkitRTCPeerConnection.setRemoteDescription = function(sd) {
+	return pcManagerJS.call_method(PCManagerJS.method_map['setRemoteDescription'], this.pc_id, sd);
 };
 
-webkitRTCPeerConnection.prototype.updateIce = function(param) {
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['updateIce'], this.pc_id, param);
+webkitRTCPeerConnection.updateIce = function(param) {
+	return pcManagerJS.call_method(PCManagerJS.method_map['updateIce'], this.pc_id, param);
 };
 
-webkitRTCPeerConnection.prototype.addIceCandidate = function(ice) {
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['addIceCandidate'], this.pc_id, ice);
+webkitRTCPeerConnection.addIceCandidate = function(ice) {
+	return pcManagerJS.call_method(PCManagerJS.method_map['addIceCandidate'], this.pc_id, ice);
 };
 
-webkitRTCPeerConnection.prototype.getStats = function() {
-	return PCManagerJS.prototype.call_method(pcManagerJS.method_map['getStats'], this.pc_id, {});
+webkitRTCPeerConnection.getStats = function() {
+	return pcManagerJS.call_method(PCManagerJS.method_map['getStats'], this.pc_id, {});
 };
 
 
